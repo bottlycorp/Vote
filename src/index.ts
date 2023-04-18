2adb364961b21347e2b8b32415e94ae263797a3a
Gaëtan H <gaetanhus@gmail.com>
2023-04-18 21:57:31 +0200

    chore(package.json): update dependencies feat(index.ts): replace fastify with express and add logging for webhook events

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,26 @@
 import { BColors } from "bettercolors";
 import { getStringEnv } from "./utils/env-variables";
+import Topgg from "@top-gg/sdk";
+import express from "express";
 
-const fastify = require("fastify")();
-const Topgg = require("@top-gg/sdk");
-
-const webhook = new Topgg.Webhook(getStringEnv("TOPGG_TOKEN"));
+const app = express();
 
 const colors = new BColors({
   date: {
     enabled: true,
-    format: "DD/MM/YYYY HH:mm:ss",
+    format: "DD/MM/YYYY - HH:mm:ss",
     surrounded: "[]"
   }
 });
 
-fastify.post("/dblwebhook", webhook.listener((vote) => {
-  colors.info(`User ${vote.user} voted for the bot!`);
-  return "Thanks for the vote!";
-}));
+const webhook = new Topgg.Webhook(getStringEnv("TOPGG_TOKEN"));
 
-fastify.post("/test", (req, res) => {
-  colors.success("Test");
-  res.send("Test");
-});
+app.post(
+  "/dblwebhook",
+  webhook.listener((vote) => {
+    console.log(vote.user);
+  })
+);
 
-fastify.listen({ port: 3000, host: "0.0.0.0" }, err => {
-  if (err) throw err;
-  colors.success("Server listening at http://0.0.0.0:3000");
-});
+app.listen(3000);
+colors.info("Webhook server is now listening on port http://0.0.0.0:3000");
